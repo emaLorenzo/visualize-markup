7ce70167612396fd02030471e4a27d57e4fb5e81
Ema Lorenzo <aemalorenzo@gmail.com>
2022-01-06 01:03:44 -0300

    using autoprefixer for vendor prefix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+import { resolve } from "path/posix";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   build: {
+    outDir: "./dist",
   },
+  css: {
+    postcss: {
+      plugins: [autoprefixer]
+    }
+  }
 });
